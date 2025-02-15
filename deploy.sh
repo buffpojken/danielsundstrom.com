862cba08fe518b7b58b07830add0ceb47ee9fbd8
buffpojken <daniel@sykewarrior.com>
2025-02-15 21:32:08 +0100

    version-bumps

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,3 @@
-jekyll build && cd _site && rsync -rui . sykewarrior.com:/sites/danielsundstrom.com/
+#jekyll build && cd _site && rsync -rui . sykewarrior.com:/sites/danielsundstrom.com/
+
+jekyll build && aws s3 sync _site s3://danielsundstrom.com
